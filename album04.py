f16cf3c5d125f161b8bb15efa36c54f6cd956a00
NoCode <nocodefirst@gmail.com>
2025-07-22 13:19:04 +0300

    🔧 Исправлен путь к изображениям

--- album04.py
+++ album04.py
@@ -16,7 +16,8 @@ MAKE_WEBHOOK_URL = 'https://hook.eu2.make.com/iaqn7i7659ktujenbzyh2oa75f4yxycu'
 
 # === Шляхи
 IMG_DIR = "C:/Users/volko/OneDrive/Desktop/NoCode/img_to_web_2"
-GITHUB_RAW_BASE = "https://raw.githubusercontent.com/NoCode240/insta-images-2/main/"
+GITHUB_RAW_BASE = "https://raw.githubusercontent.com/NoCode240/telegram-bot-render/main/img_to_web/"
+
 
 os.makedirs(IMG_DIR, exist_ok=True)
 
