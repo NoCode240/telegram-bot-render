3ea6bdb1e9a6d7d35515e7a37f2ed41359423ce4
NoCode <nocodefirst@gmail.com>
2025-07-22 14:53:41 +0300

    Заміна папки на папку в git

--- album04.py
+++ album04.py
@@ -15,7 +15,8 @@ api_hash = '58562f6b38ee6197d65fc16de649b238'
 MAKE_WEBHOOK_URL = 'https://hook.eu2.make.com/iaqn7i7659ktujenbzyh2oa75f4yxycu'
 
 # === Шляхи
-IMG_DIR = "img_to_web"
+IMG_DIR = os.path.join(os.getcwd(), "img_to_web")
+
 GITHUB_RAW_BASE = "https://raw.githubusercontent.com/NoCode240/telegram-bot-render/main/img_to_web/"
 
 os.makedirs(IMG_DIR, exist_ok=True)
